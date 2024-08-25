412d02f383882e56d7562dd045a165656b5502ef
Dominic.Fryman <fryman.dominic@gmail.com>
2024-08-25 07:31:26 -0400

    Add function updateDigitDisplay()

--- javascript.js
+++ javascript.js
@@ -3,3 +3,7 @@ let register = [];
 const digitDisplay = document.querySelector(".digit-display");
 
 digitDisplay.textContent = "Testing Content";
+
+function updateDigitDisplay(register) {
+    digitDisplay.textContent = register[0];
+}
