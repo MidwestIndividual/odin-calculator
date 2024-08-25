ac84de34b1550c0d5184c095de67565f846c9e78
Dominic.Fryman <fryman.dominic@gmail.com>
2024-08-25 07:37:06 -0400

    Add event listener to orange buttons

--- javascript.js
+++ javascript.js
@@ -7,3 +7,10 @@ digitDisplay.textContent = "Testing Content";
 function updateDigitDisplay(register) {
     digitDisplay.textContent = register[0];
 }
+
+const orangeButtons = Array.from(document.querySelectorAll(".orange-button"));
+orangeButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        console.log(event);
+    });
+});
