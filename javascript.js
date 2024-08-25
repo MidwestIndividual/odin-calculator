191b70f7c6abdbbadd96c2b4862f9b673fb72a39
Dominic.Fryman <fryman.dominic@gmail.com>
2024-08-25 07:53:43 -0400

    Add event listener to buttons with delegation via color

--- javascript.js
+++ javascript.js
@@ -8,9 +8,22 @@ function updateDigitDisplay(register) {
     digitDisplay.textContent = register[0];
 }
 
-const orangeButtons = Array.from(document.querySelectorAll(".orange-button"));
+const orangeButtons = Array.from(document.querySelectorAll(".orange-button, .teal-button, .blue-button"));
 orangeButtons.forEach((button) => {
     button.addEventListener('click', (event) => {
-        console.log(event);
+        const className = event.target.className.split(' ')[0];
+        switch (className) {
+            case 'orange-button':
+                // Run Operator Function
+                break;
+            case 'teal-button':
+                // Run numeric function
+                break;
+            case 'blue-button':
+                // Run alt function
+                break;
+            default:
+                alert("Critical Error");
+        }
     });
 });
