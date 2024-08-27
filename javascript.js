db76cf4db201cca3d1da13040d0b4aef6053ba37
Dominic.Fryman <fryman.dominic@gmail.com>
2024-08-27 07:34:41 -0400

    Add four function functionality

--- javascript.js
+++ javascript.js
@@ -7,9 +7,11 @@ function updateDigitDisplay(register) {
 }
 
 function numericButton(eventTarget) {
-    let registerIndex;
+    let registerIndex = register.length == 1 ? 0 : 2;
 
-    registerIndex = register.length == 1 ? 0 : 2;
+    if (register.length == registerIndex) {
+        register.push('');
+    }
 
     if (eventTarget.textContent == '.'){
         // Call dot code
@@ -44,13 +46,52 @@ function blueButton(eventTarget) {
     }
 }
 
+function evaluate() {
+    let operator = register[1];
+    switch (operator) {
+        case '/':
+            register = [+register[0] / +register[2]];
+            break;
+        case '*':
+            register = [+register[0] * +register[2]];
+            break;
+        case '-':
+            register = [+register[0] - +register[2]];
+            break;
+        case '+':
+            register = [+register[0] + +register[2]];
+            break;
+    }
+}
+
+function operatorButton(eventTarget) {
+    let registerIndex = register.length == 1 ? 0 : 2;
+
+    switch (eventTarget.textContent){
+        case '/':
+        case '*':
+        case '-':
+        case '+':
+            if (registerIndex == 2) {
+                evaluate();
+            }
+            register[1] = eventTarget.textContent;
+            break;
+        case '=':
+            if (registerIndex == 2) {
+                evaluate();
+            }
+            break;
+    }
+}
+
 const orangeButtons = Array.from(document.querySelectorAll(".orange-button, .teal-button, .blue-button"));
 orangeButtons.forEach((button) => {
     button.addEventListener('click', (event) => {
         const className = event.target.className.split(' ')[0];
         switch (className) {
             case 'orange-button':
-                // Run Operator Function
+                operatorButton(event.target);
                 break;
             case 'teal-button':
                 numericButton(event.target);
